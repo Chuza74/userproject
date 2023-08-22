219d08a0ffdc13bb466f9d2a0e47887915c1f6ea
Chuza74 <142811758+Chuza74@users.noreply.github.com>
2023-08-22 10:29:22 +0500

    changes

--- userproject/settings.py
+++ userproject/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+AXES_COOLOFF_TIME: 2 
+AXES_FAILURE_LIMIT: 6
+AXES_RESET_ON_SUCCESS = True
